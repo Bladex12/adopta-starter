a1930743b57ed2c3992681414161ed18c6ae7052
Bladex12 <agustin.reyes.pereira18@gmail.com>
2026-06-08 16:40:07 -0400

    agregar galeria (falta botones

--- src/app/pages/galeria/galeria.page.ts
+++ src/app/pages/galeria/galeria.page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonRouterLink, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonGrid, IonRow, IonCol} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: 'galeria.page.html',
   styleUrls: ['galeria.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [RouterModule, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonCard, IonRouterLink, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonGrid, IonRow, IonCol],
 })
-export class GaleriaPage {}
+export class GaleriaPage {
+  mascotas = [];
+}
